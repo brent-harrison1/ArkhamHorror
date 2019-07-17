658e2b38ce93097b27854c538019eb21742f0ede
Brent Harrison <harrisonchristopher101@gmail.com>
2019-07-16 21:21:36 -0400

    Updated javascript to properly run function for dice simulator (and all associated html and css related to the simulator)

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-//Need to create variables, functions (diceRoll()), and loops
-//necessary to make dice-simulator.html take in all user input,
-//and output correlating images of dice as results (also to state
-//how many successes and failures (green and red respectively) based
-//on the results and cursed/blessed conditions)
-
-//document.querySelector('input[name="status"]:checked').value;
-//NEED TO USE ABOVE QUERY SELECTOR WITH A CONDITIONAL SO THAT CURSED
-//AND BLESSED SITUATIONS WILL HAVE DIFFERENT FUNCTIONS FOR OUTPUT
-//THAN REGULAR ROLLS.
+//below function for roll the dice button for dice simulator
 
 function rollDice() {
   let numDice = document.getElementById("diceNum").value;
+  let successMin = 5;
+  let blessedOption = document.getElementById("blessed");
+  let cursedOption = document.getElementById("cursed");
+  let totalSuccesses = 0;
+
+  if (blessedOption.checked) {
+    successMin = 4;
+  } else if (cursedOption.checked) {
+    successMin = 6;
+  }
 
   for (var i = 0; i < numDice; i++) {
     let diceRoll = Math.floor(Math.random() * 6) + 1;
@@ -18,7 +19,27 @@ function rollDice() {
     let img = document.createElement("img");
     img.src = "images/d" + diceRoll + ".gif";
 
+    if (diceRoll >= successMin) {
+      img.className += "successRoll";
+      totalSuccesses += 1;
+    } else {
+      img.className += "failedRoll";
+    }
+
     let src = document.getElementById("dice-img-results");
     src.appendChild(img);
   }
+
+  document.getElementById("num-success").innerHTML =
+    totalSuccesses + " successes!!!";
+
+  document.getElementById("rollDiceBtn").onclick = function() {
+    this.disabled = true;
+  };
+}
+
+//below function is for the reset button to refresh page
+
+function refreshPage() {
+  window.location.reload();
 }
